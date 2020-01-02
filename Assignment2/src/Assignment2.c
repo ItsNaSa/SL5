7924004aa78d5ed7dd2b580211dfab2b85af573b
Sahil Naphade <38909890+ItsNaSa@users.noreply.github.com>
2020-01-02 23:01:05 +0530

    Tokenization successful, problem at '\t' at first position

--- Assignment2/src/Assignment2.c
+++ Assignment2/src/Assignment2.c
@@ -18,6 +18,10 @@ struct pottab ptab[5];
 struct symtab stab[15];
 struct registers reg[4];
 
+void create_IC(char *arr[4]){
+	
+}
+
 int main(int argc,char *argv[]) {
 	init(mtab,ptab,reg);
 	char str[100];
@@ -28,16 +32,20 @@ int main(int argc,char *argv[]) {
 	FILE *input_file = NULL;
 	input_file = fopen(argv[1],"r");	//argv[1] is ALP code file
 
-	  while (fgets(str, MAXCHAR, input_file) != NULL){
-		  token = strtok(str,"\t");
-		  i = 0;
-		  while(token != NULL){
-			  token = strtok(NULL,"\t");
-			  tokens[i] = token;
-			  i++;
-			  printf("%s\n",token);
-		  }
-	  }
-
+	  	while (fgets(str, MAXCHAR, input_file) != NULL){
+			tokens[1]=tokens[2]=tokens[3]=tokens[0] = "\0";
+			token = strtok(str,"\t");
+		  	i = 0;
+		  	while(token != NULL){
+				tokens[i] = token;
+				i++;
+				// printf("%s\n",token);
+				token = strtok(NULL,"\t");
+		  	}
+			printf("0. %s, 1. %s, 2. %s, 3. %s\n",tokens[0],tokens[1],tokens[2],tokens[3]);
+			create_IC(tokens);
+	  	}
+	
 	return EXIT_SUCCESS;
 }
+
